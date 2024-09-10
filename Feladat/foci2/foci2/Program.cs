31ed005ace248ada616cf855145836696da7ee91
Czövek Dominik Noel <czovek.dominik.noel@burg.lo.hu>
2024-09-10 13:51:46 +0200

    beolvasas

---
+++ Feladat/foci2/foci2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace foci2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var lines =File.ReadLines("meccsek.txt").ToList();
+        }
+    }
+}
---
+++ Feladat/foci2/foci2/foci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foci2
+{
+    internal class foci
+    {
+        public foci(int fordulo, int hazaigol, int vendeggol, int hazaifelido, int vendegfelido, string hazainev, string vendegnev)
+        {
+            Fordulo = fordulo;
+            Hazaigol = hazaigol;
+            Vendeggol = vendeggol;
+            Hazaifelido = hazaifelido;
+            Vendegfelido = vendegfelido;
+            Hazainev = hazainev;
+            Vendegnev = vendegnev;
+        }
+
+        public int Fordulo {  get; set; }
+        public int Hazaigol { get; set; }
+        public int Vendeggol { get; set; }
+        public int Hazaifelido { get; set; }
+        public int Vendegfelido { get; set; }
+        public string Hazainev  { get; set; }
+        public string Vendegnev { get; set; }
+    }
+    
+    
+}
